0f5626b52de753cf36d07be87dbe56e4368936c2
Irvan Putra <irvan.putra@gmail.com>
2013-08-06 05:29:59 +0000

    Save to latest

--- grader.js
+++ grader.js
@@ -22,9 +22,10 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('util');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -44,8 +45,8 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(html, checksfile) {
+    $ = cheerioHtmlFile(html);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -63,12 +64,33 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks [check_file]', 'Path to JSON file for list of checking. Default to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to HTML file to be checked.', clone(assertFileExists))
+        .option('-u, --url <url_address>', 'URL Address to be checked.')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.file !== undefined) {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
+    if (program.url !== undefined) {
+      rest.get(program.url.toString()).on('complete', function(result) {
+        if (result instanceof Error) {
+          console.log("%s does not exist. Exiting.", program.url.toString());
+          process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        } else {
+          $ = cheerio.load(result);
+          var checks = loadChecks(program.checks).sort();
+          var out = {};
+          for(var ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+          }
+          var outJson = JSON.stringify(out, null, 4);
+          console.log(outJson);        
+        }
+      });
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
